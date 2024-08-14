342541b39f6e13457741919bba421c0b623da393
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:26:15 +0300

    question 6 solved

---
+++ src/odevler/bolum01/Q06.java
@@ -0,0 +1,14 @@
+package odevler.bolum01;
+
+
+
+public class Q06 {
+    public static void main(String[] args) {
+
+        int series = 1+2+3+4+5+6+7+8+9;
+
+        System.out.println(series);
+
+    }
+
+}

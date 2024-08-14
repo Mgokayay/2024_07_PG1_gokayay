61c8473ff69ccbf2f41efee9599d0865c89b7e8e
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:27:00 +0300

    question 12 solved

---
+++ src/odevler/bolum01/Q12.java
@@ -0,0 +1,11 @@
+package odevler.bolum01;
+
+public class Q12 {
+    public static void main(String[] args) {
+       double x = 24 * 1.6;
+       double t = 1+(40.35/60);
+       double aV = x/t;
+       System.out.println(aV);
+
+    }
+}

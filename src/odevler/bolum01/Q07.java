17e58b77c075a81931aa86f393e5b8a8a6bf1893
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:26:21 +0300

    question 6 solved

---
+++ src/odevler/bolum01/Q07.java
@@ -0,0 +1,11 @@
+package odevler.bolum01;
+
+public class Q07 {
+    public static void main(String[] args) {
+
+        double result = 4 *(1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11);
+        System.out.println(result);
+        double result2 = 4 *(1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11+1.0/13);
+        System.out.println(result2);
+    }
+}

d99e7bff524ba63be4f178cf7ecee53017e7db2a
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:26:00 +0300

    question 4 solved

---
+++ src/odevler/bolum01/Q04.java
@@ -0,0 +1,18 @@
+package odevler.bolum01;
+
+import java.util.Scanner;
+
+public class Q04 {
+    public static void main(String[] args) {
+        System.out.println("a   a^2   a^3");
+        int a = 1;
+        System.out.println(a+"    "+a*a+"    "+a*a*a);
+        a=2;
+        System.out.println(a+"    "+a*a+"    "+a*a*a);
+        a=3;
+        System.out.println(a+"    "+a*a+"    "+a*a*a);
+        a=4;
+        System.out.println(a+"    "+a*a+"   "+a*a*a);
+
+    }
+}

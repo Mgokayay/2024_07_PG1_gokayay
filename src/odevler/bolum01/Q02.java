5a96fdb67f12675c0718a599d0b9482535319a4c
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:25:47 +0300

    question 2 solved

--- src/odevler/bolum01/Q02.java
+++ src/odevler/bolum01/Q02.java
@@ -1,4 +1,12 @@
 package odevler.bolum01;
 
 public class Q02 {
+    public static void main(String[] args) {
+        System.out.println("Javaya hoşgeldiniz");
+        System.out.println("Javaya hoşgeldiniz");
+        System.out.println("Javaya hoşgeldiniz");
+        System.out.println("Javaya hoşgeldiniz");
+        System.out.println("Javaya hoşgeldiniz");
+
+    }
 }

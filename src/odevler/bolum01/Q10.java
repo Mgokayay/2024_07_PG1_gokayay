76784a3d4600225dae793c9b53ee5bb3419c4ba7
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:26:44 +0300

    question 10 solved

---
+++ src/odevler/bolum01/Q10.java
@@ -0,0 +1,13 @@
+package odevler.bolum01;
+
+public class Q10 {
+    public static void main(String[] args) {
+       double t = 45.5;
+       double v = 14;
+       double m = 1.6;
+       double h = 60;
+
+       System.out.println((v/m)/(t/h));
+
+    }
+}

363bb070b9a4425fdd219f91db60b265b8457e63
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-14 17:26:37 +0300

    question 9 solved

---
+++ src/odevler/bolum01/Q09.java
@@ -0,0 +1,11 @@
+package odevler.bolum01;
+
+public class Q09 {
+    public static void main(String[] args) {
+        double weight = 4.5;
+        double height = 7.9;
+
+        double area = weight * height;
+        System.out.println("The area is " + area);
+    }
+}

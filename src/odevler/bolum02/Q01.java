c404f719ddd483aaacb23d2b2a2ca316b5724da8
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-08-20 17:25:20 +0300

    chapter 2 question 1 solved

--- src/odevler/bolum02/Q01.java
+++ src/odevler/bolum02/Q01.java
@@ -1,7 +1,16 @@
 package odevler.bolum02;
 
+import java.util.Scanner;
+
 public class Q01 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a degree in Celsius ");
+        double celsius = scanner.nextDouble();
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.printf((double)celsius + " Celsius " + fahrenheit + "Fahrenheit");
 
     }
 }
